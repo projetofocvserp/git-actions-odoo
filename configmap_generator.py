cc525a2418cca6886d8bc9f40c2bc31c4a35db2d
Marcelo Marcon <marcelo.marcon@ax4b.com>
2023-07-25 10:34:17 -0400

    implementado fx para gerar submodulos de acordo com a pasta extra-addons e enviar para o novo configmap

--- configmap_generator.py
+++ configmap_generator.py
@@ -1,29 +1,42 @@
-import yaml
-from io import StringIO
 import configparser
+from io import StringIO
+from pathlib import Path
+from typing import Any, Dict
 
-with open('config_map.yaml', 'r') as f:
-    data = yaml.safe_load(f)
-
-ini_str = data['data']['odoo.conf']
-ini_file = StringIO(ini_str)
-config = configparser.ConfigParser()
-config.read_file(ini_file)
-
-config.set('options', 'addons_path',
-           '/usr/lib/python3/dist-packages/odoo/addons, /mnt/extra-addons/sessions, /mnt/extra-addons/server-tools')
-
-with StringIO() as ini_file:
-    config.write(ini_file)
-    ini_str_updated = ini_file.getvalue()
-
-data['data']['odoo.conf'] = ini_str_updated
+import yaml
 
 
-def save_to_configmap_file() -> None:
+def get_submodules_from_extra_addons_folder() -> str:
+    BASE = Path('extra-addons')
+    submodule_paths = ["/mnt/" + str(module)
+                       for module in BASE.glob('*') if module.is_dir()]
+    return ', '.join(submodule_paths)
+
+
+def generate_new_configmap() -> Dict[str, Any]:
+    with open('config_map.yaml', 'r') as f:
+        data = yaml.safe_load(f)
+
+    ini_str = data['data']['odoo.conf']
+    ini_file = StringIO(ini_str)
+    config = configparser.ConfigParser()
+    config.read_file(ini_file)
+
+    config.set('options', 'addons_path',
+               get_submodules_from_extra_addons_folder())
+
+    with StringIO() as ini_file:
+        config.write(ini_file)
+        ini_str_updated = ini_file.getvalue()
+
+    data['data']['odoo.conf'] = ini_str_updated
+    return data
+
+
+def save_to_configmap_file(data: Dict[str, Any]) -> None:
     with open('config_map.yaml', 'w') as f:
         yaml.safe_dump(data, f)
 
 
 if __name__ == '__main__':
-    save_to_configmap_file()
+    save_to_configmap_file(data=generate_new_configmap())
